2ef2edb323643d990a9adab6176e548c14a6270a
NathanDzreke-Poku <dzrekenathan2002@gmail.com>
2024-06-26 19:57:21 +0000

    upgrade

---
+++ app/src/main/java/com/example/securityallocator/JobAssignDetails.java
@@ -0,0 +1,15 @@
+package com.example.securityallocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobAssignDetails extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_assign_details);
+    }
+}
